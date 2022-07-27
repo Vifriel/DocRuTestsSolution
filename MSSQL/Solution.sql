731533a03994e34179084d328f54c2bfe6d50ac2
Vifriel <algadarraimok@gmail.com>
2022-07-27 19:47:37 +0300

    Fix SQL mistake for the 4th task and clean up group by statements for other queries

--- MSSQL/Solution.sql
+++ MSSQL/Solution.sql
@@ -7,19 +7,20 @@ where Cars.[BaseID] is not null and Cars.[BrandID] = parents.[BrandID];
 select Brands.[Name]
 from Cars
 left join Brands on Cars.[BrandID] = Brands.[ID]
-group by Cars.[BrandID], Brands.[Name]
+group by Brands.[Name]
 having count(*) > 3;
 --3rd Task
 select Brands.[Name], SUM(PRICE) as 'Price Summary'
 from Cars
 left join Brands on Cars.[BrandID] = Brands.[ID]
-group by Cars.[BrandID], Brands.[Name];
+group by Brands.[Name];
 --4th Task
-select TOP (2) Brands.[Name], AVG(Pow) as 'Average Power'
+select TOP (2) Countries.[Name], Round(AVG(CAST(Pow as float)), 2) as 'Average Power'
 from Cars
 left join Brands on Cars.[BrandID] = Brands.[ID]
-group by Cars.[BrandID], Brands.[Name]
-order by AVG(Pow) desc;
+left join Countries on Brands.[CountryID] = Countries.[ID]
+group by Countries.[Name]
+order by AVG(CAST(Pow as float)) desc;
 --5th Task
 select o.[Name] as 'Car Name', Brands.[Name] as 'Brand Name'
 from Cars o                    
